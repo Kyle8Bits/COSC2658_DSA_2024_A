b1113a5ae32f403603545bb6574b99485a51ca6e
Kyle8Bit <khoamaidang2611@gmail.com>
2024-05-06 22:24:28 +0700

    delete later

---
+++ PlaceGenerator.java
@@ -0,0 +1,47 @@
+import java.util.Random;
+
+public class PlaceGenerator {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        char tick = '"';
+        // Generate 100 Place objects
+        for (int i = 0; i < 100; i++) {
+            int x = random.nextInt(7878000 - 1000000 + 1) + 1000000;
+            int y = random.nextInt(7878000 - 1000000 + 1) + 1000000;
+            String name = generateRandomName();
+            String[] services = generateRandomServices();
+            // Do something with the Place object, like printing it
+            System.out.println("new Place("+x+"," + y +"," + tick + name + tick + ", new String[] {" + printArry(services) + "}),");
+        }
+    }
+
+    private static String printArry(String[] service){
+        String result = "";
+        char tick = '"';
+        for (int i  = 0; i < service.length; i++){
+            result += tick + service[i] + tick + ",";
+        }
+        return result;
+    }
+
+    private static String generateRandomName() {
+        String[] names = { "Park", "Restaurant", "Museum", "School", "Hospital",
+                "Cafe", "Shop", "Theater", "Gym", "Library", "Community Center", 
+                "Botanical Garden", "Art Gallery", "Town Square", "Recreational Center", 
+                "Visitor Center", "Cultural Center", "Exhibition Hall", "Amphitheater", "Observatory" };
+        Random random = new Random();
+        return names[random.nextInt(names.length)];
+    }
+
+    private static String[] generateRandomServices() {
+        String[] services = new String[5];
+        String[] availableServices = { "Food", "Drinks", "Entertainment", "Education",
+                "Healthcare", "Shopping", "Fitness", "Art", "Community","Transportation"};
+        Random random = new Random();
+        for (int i = 0; i < 5; i++) {
+            services[i] = availableServices[random.nextInt(availableServices.length)];
+        }
+        return services;
+    }
+}
