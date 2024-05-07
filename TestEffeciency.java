f0a1cb85417c9c99fff7d6af2fb35dda8baf4dcd
TaiVanNgo <vantaingo.056@gmail.com>
2024-05-07 17:32:16 +0700

    push

--- TestEffeciency.java
+++ TestEffeciency.java
@@ -1,46 +1,58 @@
 import java.util.Random;
 
 public class TestEffeciency {
-
     public static void main(String[] args) {
         Random random = new Random();
-        int size = 100000;
+        int size = 10000000;
         // Generate 100 Place objects
         Place places[] = new Place[size];
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < size - 1; i++) {
             int x = random.nextInt(10000000);
             int y = random.nextInt(10000000);
             String name = generateRandomName();
             String[] services = generateRandomServices();
             places[i] = new Place(x, y, name, services);
         }
-
+        places[size - 1] = new Place(999999, 999999, "removePlace", new String[] {"removeService"});
         Map2D map = new Map2D();
 
-        long addStart = System.currentTimeMillis();
+        //ADDING TEST TIME
+        // long addStart = System.currentTimeMillis();
         map.add(places);
-        System.out.println("Add time : " + (double) (System.currentTimeMillis() - addStart));
+        System.out.println("Map size: " + map.tree.size());
+        // System.out.println("Adding time : " + (System.currentTimeMillis() - addStart) + "(ms)");
 
-        System.out.println("Map size: " +  map.tree.size());
+        //EDIT TEST TIME
+        long editStart = System.nanoTime();
+        map.edit(99999, 91112, new String[] {"edit service", "edit new"});
+        System.out.println("Edit time : " + (System.nanoTime() - editStart) + "(nano time)");
+        
+        //REMOVE TEST TIME
+        // long removeStart = System.nanoTime();
+        // map.remove(99999, 91112);
+        // System.out.println("Remove time : " + (System.nanoTime() - removeStart) + "(nano time)");
 
-        long addSearch = System.currentTimeMillis();
-        PlaceList foundPlaces = map.searchPlaces(5000000, 5000000, 50000, 50000, "Food");
-        System.out.println("Add: " + (double) (System.currentTimeMillis() - addSearch));
-
+        // long addSearch = System.currentTimeMillis();
+        // PlaceList foundPlaces = map.searchPlaces(5000000, 5000000, 50000, 50000,
+        // "Food");
+        // System.out.println("Add: " + (double) (System.currentTimeMillis() -
+        // addSearch));
 
         // if (foundPlaces.isEmpty()) {
-        //     System.out.println("No places found within the specified area that offer the service: "
-        //                     );
+        // System.out.println("No places found within the specified area that offer the
+        // service: "
+        // );
         // } else {
-        //     System.out.println("Found " + foundPlaces.getSize() + " places offering " + "Food" + "' within the area.");
-        //     System.out.println(foundPlaces.toString(5000000, 5000000));
+        // System.out.println("Found " + foundPlaces.getSize() + " places offering " +
+        // "Food" + "' within the area.");
+        // System.out.println(foundPlaces.toString(5000000, 5000000));
         // }
     }
 
     private static String generateRandomName() {
         String[] names = { "Park", "Restaurant", "Museum", "School", "Hospital",
-                "Cafe", "Shop", "Theater", "Gym", "Library", "Community Center", 
-                "Botanical Garden", "Art Gallery", "Town Square", "Recreational Center", 
+                "Cafe", "Shop", "Theater", "Gym", "Library", "Community Center",
+                "Botanical Garden", "Art Gallery", "Town Square", "Recreational Center",
                 "Visitor Center", "Cultural Center", "Exhibition Hall", "Amphitheater", "Observatory" };
         Random random = new Random();
         Random random2 = new Random();
@@ -52,10 +64,10 @@ public class TestEffeciency {
     private static String[] generateRandomServices() {
         String[] services = new String[5];
         String[] availableServices = { "Food", "Drinks", "Entertainment", "Education",
-                "Healthcare", "Shopping", "Fitness", "Art", "Community","Transportation"};
+                "Healthcare", "Shopping", "Fitness", "Art", "Community", "Transportation" };
         Random random = new Random();
         int start = random.nextInt(availableServices.length - 5);
-        
+
         for (int i = 0; i < 5; i++) {
             services[i] = availableServices[start];
             start++;
